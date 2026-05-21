7c69e379908f6d1a3aeedbbef1afd4b132706ef5
Raul Somesan <raulfabiansome@gmail.com>
2026-05-21 16:25:38 +0300

    ENV variables

--- src/public/index.php
+++ src/public/index.php
@@ -2,9 +2,21 @@
 
 declare(strict_types=1);
 
+use App\App;
+use App\Router;
+use App\View;
+
 //include the autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
 
 //load .env variables
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
+
+//define paths
+define('VIEW_PATH', '../views');
+define('STORAGE_PATH', '../storage');
+
+echo View::make('transactions');
+
+//route the application
